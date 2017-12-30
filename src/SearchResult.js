10f10c54e707d086d8577969734346754c5816e7
Peter Vestlin <peter.vestlin@lm.se>
2017-12-30 11:08:33 +0100

    Refact

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -3,8 +3,9 @@ import noImage from './no_image.png';
 
 export const SearchResult = ({movie}) => (
    <div className="SearchResult">
-      <h4>Title: {movie.title} ({movie.year})</h4>
-      <p>{movie.type}</p>
+      <h4>{movie.title}</h4>
+      <p><b>Year</b>: {movie.year}</p>
+      <p><b>Type</b>: {movie.type}</p>
       <img src={movie.poster !== "N/A" ? movie.poster : noImage} alt="Missing" height="100"/>
    </div>
 );
