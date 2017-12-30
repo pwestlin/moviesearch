de53038b6aeddff02c89ec6157893b36a668b42b
Peter Vestlin <peter.vestlin@lm.se>
2017-12-30 10:01:04 +0100

    Refactoring.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import logo from './movie.png';
 import './App.css';
-import SearchResult from "./SearchResult";
+import {SearchResult} from "./SearchResult";
 
 class App extends Component {
 
--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import noImage from './no_image.png';
 
-function SearchResult(props) {
-   const movie = props.movie;
-   return (
-      <div className="SearchResult">
-         <h4>Title: {movie.title} ({movie.year})</h4>
-         <img src={movie.poster !== "N/A" ? movie.poster : noImage} alt="No image" height="100"/>
-      </div>
-   );
-}
-
-export default SearchResult;
+export const SearchResult = ({movie}) => (
+   <div className="SearchResult">
+      <h4>Title: {movie.title} ({movie.year})</h4>
+      <img src={movie.poster !== "N/A" ? movie.poster : noImage} alt="Missing" height="100"/>
+   </div>
+);
